84a2851c22856fba0e7471dd8261a2135c9b7067
KPicart <kpicart@stepwisedesigns.com>
2016-08-30 23:07:54 -0400

    viewing gameLogic

--- matchBuild02/matchBuild02/CardGameViewController.m
+++ matchBuild02/matchBuild02/CardGameViewController.m
@@ -9,10 +9,21 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *touchCardFlipLabel;
+@property (nonatomic)int flipCount;
 
 @end
 
+
+
 @implementation CardGameViewController
+- (IBAction)touchCardButton:(UIButton *)sender {
+    
+    [sender setBackgroundImage:[UIImage imageNamed:@"cardFront"] forState:UIControlStateNormal];
+    [sender setTitle:@"A♦️" forState:UIControlStateNormal];
+    
+    
+}
 
 
 @end
