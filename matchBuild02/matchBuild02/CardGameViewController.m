d2d4fde7c3f2d5a7c3f7f32f75778b88fef86c3b
KPicart <kpicart@stepwisedesigns.com>
2016-08-31 23:52:01 -0400

    cardMatching review

--- matchBuild02/matchBuild02/CardGameViewController.m
+++ matchBuild02/matchBuild02/CardGameViewController.m
@@ -7,22 +7,50 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *touchCardFlipLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *flipLabelText;
 @property (nonatomic)int flipCount;
+@property (strong,nonatomic)PlayingCardDeck *deck;
+
 
 @end
 
 
-
 @implementation CardGameViewController
-- (IBAction)touchCardButton:(UIButton *)sender {
+
+-(PlayingCardDeck *)deck{
+    if(!_deck) _deck = [self createDeck];
+    return _deck;
+}
+
+-(PlayingCardDeck *)createDeck{
+    return [PlayingCardDeck new];
+}
+
+
+-(void)setFlipCount:(int)flipCount{
     
+    _flipCount = flipCount;
+    self.flipLabelText.text = [NSString stringWithFormat:@"Flip Count %d",self.flipCount];
+}
+
+
+-(IBAction)flipTouchCard:(UIButton*)sender{
+    if([sender.currentTitle length]){
+        
+    [sender setBackgroundImage:[UIImage imageNamed:@"subzeroCardBack"] forState:UIControlStateNormal];
+    [sender setTitle:@"" forState:UIControlStateNormal];
+    }
+    else {
+        Card *randomCard = [self.deck drawRandomCard];
     [sender setBackgroundImage:[UIImage imageNamed:@"cardFront"] forState:UIControlStateNormal];
-    [sender setTitle:@"A♦️" forState:UIControlStateNormal];
+    [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+}
     
-    
+    self.flipCount++;
 }
 
 
